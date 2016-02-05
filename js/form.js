7c83a885c37bb1bfd244d2f7d6ac49c5c8b57f1c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2016-02-05 22:44:58 +0400

    module3-task2

--- js/form.js
+++ js/form.js
@@ -18,7 +18,6 @@
 
 
   var buttonReviewSubmit = document.getElementsByClassName('review-submit')[0];
-  debugger;
   buttonReviewSubmit.onsubmit = function(evt) {
     evt.preventDefault();
     var reviewMark = document.querySelectorAll('.review-form-group-mark input[checked]')[0].getAttribute('value');

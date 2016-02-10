ee850752df4ebe70823d46cf05186547ecf26265
Vakhtang Tivadze <vaxotivadze@gmail.com>
2016-02-10 13:03:30 +0400

    module3-task2

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+/* global docCookies: true */
 'use strict';
 
 (function() {
@@ -17,13 +18,34 @@
 
 
 
-  var buttonReviewSubmit = document.getElementsByClassName('review-submit')[0];
-  debugger;
-  buttonReviewSubmit.onsubmit = function(evt) {
+  var form = document.forms['review-form'];
+  var buttonReviewSubmit = form.elements['review-submit'];
+
+  buttonReviewSubmit.onclick = function(evt) {
     evt.preventDefault();
-    var reviewMark = document.querySelectorAll('.review-form-group-mark input[checked]')[0].getAttribute('value');
-    var reviewName = document.querySelector('#review-name').value;
-    debugger;
+
+    var reviewMarks = form['review-marks'].elements;
+    for (var i = 0; i < reviewMarks.length; i++) {
+      if (reviewMarks[i].checked === true) {
+        var reviewMark = reviewMarks[i].value;
+        break;
+      }
+    }
+    var reviewName = form['review-name'].value;
+
+    var BIRTHDAY_MONTH = 3;
+    var BIRTHDAY_DAY = 1;
+
+    var today = new Date();
+    var curYear = today.getFullYear();
+    var curYearBirtday = new Date(curYear, BIRTHDAY_MONTH, BIRTHDAY_DAY);
+    var lastBirthday = today > curYearBirtday ? curYearBirtday : new Date(curYear - 1, BIRTHDAY_MONTH, BIRTHDAY_DAY);
+
+    var expires = new Date(+today + +today - +lastBirthday);
+
+    docCookies.setItem('reviewMark', reviewMark, expires);
+    docCookies.setItem('reviewName', reviewName, expires);
+
   };
 
 })();
